fd13c99c6b770c998a9590dc0d0671f96dc4f2c2
Amdadul Haque <amdadulhaquekhan500@gmail.com>
2026-06-04 19:15:39 +0600

    first commit

---
+++ database/migrations/2026_06_04_125340_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('students', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->string('border_number')->unique();
+        $table->string('roll_number')->unique();
+        $table->string('name');
+        $table->string('department');
+        $table->string('session');
+        $table->string('phone')->nullable();
+        $table->foreignId('room_id')->nullable()->constrained()->onDelete('set null');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
